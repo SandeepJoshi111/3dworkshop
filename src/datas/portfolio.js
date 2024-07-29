28f63c839cb68cdfb81be541844361af108fa8c9
Sandeep Joshi <joshisandip111@gmail.com>
2024-07-29 11:02:08 +0545

    portfolio page

---
+++ src/datas/portfolio.js
@@ -0,0 +1,88 @@
+const portfolios = [
+  {
+    image: "/public/images/portfolios/Borsa Black (Front View).png",
+  },
+  {
+    image: "/public/images/portfolios/Borsa Cream (Front View).png",
+  },
+  {
+    image: "/public/images/portfolios/Borsa Green (Front View).png",
+  },
+  {
+    image: "/public/images/portfolios/Black Pink.png",
+  },
+  {
+    image: "/public/images/portfolios/Brown Green.png",
+  },
+  {
+    image: "/public/images/portfolios/White Black.png",
+  },
+  {
+    image: "/public/images/portfolios/airfryer.png",
+  },
+  {
+    image: "/public/images/portfolios/bottle.png",
+  },
+  {
+    image: "/public/images/portfolios/cooker.png",
+  },
+  {
+    image: "/public/images/portfolios/mixer.png",
+  },
+  {
+    image: "/images/portfolios/1. Room.png",
+  },
+  {
+    image: "/public/images/portfolios/12.Lipstick.png",
+  },
+  {
+    image: "/public/images/portfolios/13.Cream.png",
+  },
+  {
+    image: "/public/images/portfolios/14.Black Water Denoise.png",
+  },
+  {
+    image: "/public/images/portfolios/16. Momo Drish.png",
+  },
+  {
+    image: "/public/images/portfolios/16. Momo.png",
+  },
+  {
+    image: "/public/images/portfolios/17.Headphone White.png",
+  },
+  {
+    image: "/public/images/portfolios/18.Airpods.png",
+  },
+  {
+    image: "/public/images/portfolios/22.Chess.png",
+  },
+  {
+    image: "/public/images/portfolios/30minhouse.png",
+  },
+  {
+    image: "/public/images/portfolios/8.Victoria.png",
+  },
+  {
+    image: "/public/images/portfolios/astronaut.png",
+  },
+  {
+    image: "/public/images/portfolios/CardBoard.png",
+  },
+  {
+    image: "/public/images/portfolios/iphone11promax.png",
+  },
+  {
+    image: "/public/images/portfolios/PastryFocus.png",
+  },
+  {
+    image: "/public/images/portfolios/penguin.png",
+  },
+  {
+    image: "/public/images/portfolios/rocket.png",
+  },
+  {
+    image: "/public/images/portfolios/TelephoneBlur.png",
+  },
+];
+
+export default portfolios;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Button from "../components/Button";
+import portfolios from "../datas/portfolio";
+import LazyLoad from "react-lazyload";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
     <div className="my-10">
-      <Button title={"Back"} />
-      <div>
-        <p>Some of the portfolio projects</p>
+      <Link to={"/"}>
+        <Button title={"Back"} />
+      </Link>
+      <div className="mt-10">
+        <div className="grid grid-cols-3 gap-4">
+          {portfolios.map((portfolio) => (
+            <div key={portfolio.id} className="w-full">
+              <LazyLoad height={200} offset={100}>
+                <img
+                  src={portfolio.image}
+                  alt={`Portfolio ${portfolio.id}`}
+                  className="w-[500px] h-[400px] object-cover "
+                  loading="lazy"
+                />
+              </LazyLoad>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
